2da2e42c9cc209f3d7e2cfc836516c3de30457b9
Mantaci <mantacifery@gmail.com>
2024-09-14 14:40:17 +0300

    2

---
+++ api-test/src/test/java/ApiTests.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ApiTests {
+
+    @Test
+    @Tag("Smoke")
+    public void some_test(){
+
+    }
+
+    @Test
+    public void some_test2(){
+
+    }
+
+    @Test
+    public void some_test3(){
+
+    }
+
+}
---
+++ ui-test/src/test/java/UiTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class UiTests {
+
+    @Test
+    public void some_test(){
+
+    }
+
+    @Test
+    @Tag("Smoke")
+    public void some_test2(){
+
+    }
+}
